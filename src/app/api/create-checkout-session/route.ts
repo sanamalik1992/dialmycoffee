38a4d8c3ec08a6064019b26a02e8eeecc3f29558
sanamalik1992 <6ktbg8vz6q@privaterelay.appleid.com>
2026-01-01 19:59:36 +0000

    Update Stripe API version

--- src/app/api/create-checkout-session/route.ts
+++ src/app/api/create-checkout-session/route.ts
@@ -8,7 +8,7 @@ const supabase = createClient(
 );
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-11-20.acacia',
+apiVersion: '2025-12-15.clover',
 });
 
 export async function POST(req: NextRequest) {
--- src/app/api/stripe-webhook/route.ts
+++ src/app/api/stripe-webhook/route.ts
@@ -8,7 +8,7 @@ const supabase = createClient(
 );
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-11-20.acacia',
+ apiVersion: '2025-12-15.clover',
 });
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
